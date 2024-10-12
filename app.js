7908fb0989611105c95cba06c8f55e5ec6bbd371
arttturslv <arttturslv@gmail.com>
2024-10-12 19:18:52 -0300

    feat: added routes for url methods

--- app.js
+++ app.js
@@ -1,18 +1,74 @@
 import express from "express";
 import { config as env_config } from "dotenv";
 import mongoose from "mongoose";
+import urlScheme from './models/url.js'
 
 env_config();
 mongoose.connect(process.env.MONGO_DB);
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return "Hello World."
 });
 
+app.post('/', async (req, res) => {
+    try {
+        const urlOriginal = req.body.URL;
+        console.log(urlOriginal)
+        
+        if(!urlOriginal) {
+            console.log("Não é possivel completar a requisição, falta o paramentro 'URL': ", urlOriginal);
+            return res.sendStatus(400);
+        }
 
-app.listen(process.env.PORT || 3333);
+        if(!isURLValid(urlOriginal)) {
+            console.log("Não é possivel completar a requisição, a URL é inválida: ", urlOriginal);
+            return res.sendStatus(400);
+        }
+
+        const url = await urlScheme.create({original:urlOriginal});
+        res.send(url);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400)
+    }
+});
+
+app.get('/:urlEncurtada', async (req, res) => {
+    try {
+        const urlEncurtada = req.params.urlEncurtada;
+
+        const url = await urlScheme.findOne({ encurtado: urlEncurtada});
+    
+        if(!url) {
+            console.log("Não existe a URL encurtada: ", urlEncurtada);
+            return res.sendStatus(404);
+        }
+    
+        url.visitas++;
+        url.save();
+        res.send(url);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400)
+    }
+});
+
+app.listen(process.env.PORT || 3333, () => {
+    console.log("Server is on fire.")
+});
 
 
-
+function isURLValid(url) {
+    try {
+        const urlRecebida = url.startsWith('http://') || url.startsWith('https://') ? url : `http://${url}`;
+        new URL(urlRecebida)
+        return true;
+    } catch {
+        return false;
+    }
+}
